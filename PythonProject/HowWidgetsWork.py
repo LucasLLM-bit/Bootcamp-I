6af313f3bd758205bb7c8477feb87c512033b262
LucasNotz <lucas.fontana@sempreceub.com>
2025-05-09 16:31:57 -0300

    Finishing touch for the moment

--- PythonProject/HowWidgetsWork.py
+++ PythonProject/HowWidgetsWork.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 
+def button_func():
+    #get content
+    entry_text = entry.get()
+    #update label
+    label['text'] = entry_text
+    entry['state'] = 'disabled'
+
+
 #window
 window = tk.Tk()
 window.title('Getting and setting Widgets')
@@ -15,7 +23,7 @@ entry.pack(pady = 5)
 
 button = ttk.Button(master = window,
                     text = "Button",
-                    command = lambda: print("button clicked"))
+                    command = button_func)
 button.pack(pady = 10)
 
 #run
